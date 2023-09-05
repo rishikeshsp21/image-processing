4a436ab0c022554d95e5e540affd00f7f4a56d60
rishikeshsp21 <rishikeshsp21@gmail.com>
2023-09-05 11:13:30 +0530

    worked on fashion mnist

--- get rid.py
+++ get rid.py
@@ -1,5 +1,10 @@
 import os
+import time
 path = "C:\\Users\\rishi\\Documents\\image processing\\gan output images"
 l = []
 x = os.listdir(path)
 print(len(x))
+# for i in range(len(x)):
+#     if((i + 1) % 25 != 0):
+#         print("deleted file : ", path +"\\" +  x[i])
+#         os.remove(path +"\\" +  x[i])
